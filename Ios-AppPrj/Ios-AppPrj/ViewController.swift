bc97709b189dd418f279fa185d3fe676d487d6bf
Hieupd <Hieupd@Hieupds-iMac.local>
2017-05-18 19:47:13 +0700

    init proj

---
+++ Ios-AppPrj/Ios-AppPrj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ios-AppPrj
+//
+//  Created by Hieupd on 5/18/17.
+//  Copyright © 2017 com.Hieupd.demo1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
