e5c624cc2aaefdbe37571df3fd0eeeb5e1c8147f
duchieuna <duchieuna@gmail.com>
2017-05-23 21:35:58 +0700

    userdefault

--- Ios-AppPrj/Ios-AppPrj/ViewController.swift
+++ Ios-AppPrj/Ios-AppPrj/ViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textfield_username: UITextField!
 
+    @IBOutlet weak var textfield_password: UITextField!
     override func viewDidLoad() {
+        //load defaule
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //get standard userdefault
+        let ud = UserDefaults.standard
+        //read in for key lan_chay
+        var lanChay = ud.integer(forKey: "lan_chay")
+        print("Lan chay thu \(lanChay)")
+        //increase lan_chay by 1
+        lanChay = lanChay + 1
+        //write lan_chay to userdefault
+        ud.set(lanChay, forKey: "lan_chay")
+        //Synchronize -> Commit
+        ud.synchronize()
+        
+        var username = UserDefaults.standard
+        var landangnhap = username.integer(forKey: "lan_dang_nhap")
+        landangnhap = landangnhap + 1
+        username.set(landangnhap, forKey: "lan_dang_nhap")
+        username.synchronize()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func dang_nhap() {
+        
+        
     }
     @IBAction func actioncham(_ sender: UITapGestureRecognizer) {
         print("abc")
